fc7456281e0c13b15b5b0a9be3d91c5bea34da07
Marshal Immanuel (V) <marshal_sudhan@apple.com>
2018-01-08 14:20:52 -0800

    Initial commit

---
+++ src/main/java/marsh/json_simple/App.java
@@ -0,0 +1,11 @@
+package marsh.json_simple;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+	public static void main(String[] args) {
+		new JSONEncode();
+	}
+}
---
+++ src/main/java/marsh/json_simple/JSONEncode.java
@@ -0,0 +1,42 @@
+package marsh.json_simple;
+
+import java.util.HashMap;
+
+import org.json.simple.JSONObject;
+import org.json.simple.JSONValue;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+public class JSONEncode {
+
+	public JSONEncode() {
+		HashMap<Object, Object> jsonObject = new HashMap<Object, Object>();
+		jsonObject.put("name", "Marshal");
+		jsonObject.put("age", new Integer(33));
+		jsonObject.put("address", "India");
+		jsonObject.put("salary", new Double(80000));
+		jsonEncode(jsonObject);
+
+		String jsonString = "{\"name\":\"Marshal\",\"age\":33,\"address\":\"India\"}";
+		jsonDecode(jsonString);
+	}
+
+	public void jsonEncode(HashMap<Object, Object> input) {
+		System.out.println("Inside jsonEncode method.");
+		try {
+			JSONObject object = (JSONObject) new JSONParser().parse(JSONObject.toJSONString(input));
+			System.out.println(object);
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void jsonDecode(String jsonInput) {
+		System.out.println("Inside jsonDecode method.");
+		Object object = JSONValue.parse(jsonInput);
+		JSONObject jsonObject = (JSONObject) object;
+		String name = (String) jsonObject.get("name");
+		System.out.println("Name: " + name);
+	}
+
+}
